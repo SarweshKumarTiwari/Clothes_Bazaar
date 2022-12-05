f7ca3b8996acf68de25e3a5487f680c79e294c45
Sarwesh <sarweshtiwari14@gmail.com>
2022-12-05 20:34:23 +0530

    Updated Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,6 +10,7 @@ import SellerAuth from './Navigation/bigscreen/SellerAuth';
 import SmallScreenCat from './Navigation/smallscreen/SmallScreenCat';
 import IsNotAuth1 from './Navigation/smallscreen/IsNotAuth';
 import IsAuth1 from './Navigation/smallscreen/IsAuth';
+import IsAuthSeller from './Navigation/smallscreen/IsAuthSeller';
 
 export default function Navbar() {
 
@@ -129,10 +130,10 @@ export default function Navbar() {
                         <SmallScreenCat pathname={loc.pathname}/>
                       </li>
                       <li>
-                        {!isAuth ? <IsNotAuth1 pathname={loc.pathname}/>:
+                        {isseller?<IsAuthSeller pathname={loc.pathname} isAuth={isseller}/>:!isAuth ? <IsNotAuth1 pathname={loc.pathname}/>:
                           <IsAuth1 isAuth={isAuth} pathname={loc.pathname}/>}
                       </li>
-                      <li className={loc.pathname === '/cart' ? "pl-4 bg-gray-300 rounded-md" : ""}>
+                      {!isseller&&<li className={loc.pathname === '/cart' ? "pl-4 bg-gray-300 rounded-md" : ""}>
                         <Link
                           to="/cart"
                           state={{ isAuth: isAuth }}
@@ -144,7 +145,7 @@ export default function Navbar() {
                             <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
                           </svg>
                         </Link>
-                      </li>
+                      </li>}
                     </ul>
 
                   </nav>
